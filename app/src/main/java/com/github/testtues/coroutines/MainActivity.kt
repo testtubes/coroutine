4d678df10f9e030ddd87a92f2bcbe5e40200271e
biwin <biwin007@gmail.com>
2020-03-01 16:00:49 +0530

    adds stuff

---
+++ app/src/main/java/com/github/testtues/coroutines/MainActivity.kt
@@ -0,0 +1,52 @@
+ package com.github.testtues.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
+ class MainActivity : AppCompatActivity() {
+
+    private val RESULT_1 = "RESULT #1"
+    private val RESULT_2 = "RESULT #2"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                fakeApiRequest()
+            }
+        }
+    }
+
+    private fun setNewText(input: String) {
+        val newText = text.text.toString() + "\n$input"
+        text.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(input: String) {
+        withContext(Dispatchers.Main) {
+            setNewText(input)
+        }
+    }
+
+    private suspend fun fakeApiRequest(){
+        val result1 = getResult1FromApi()
+        setTextOnMainThread(result1)
+        Log.i("debug", "debug coroutine working")
+    }
+
+    private suspend fun getResult1FromApi(): String{
+        logThread("debug getResult1FromApi")
+        delay(1000)
+        return RESULT_1
+    }
+
+    private fun logThread(methodName: String) {
+        Log.i("somestuff","Debug: ${methodName}: ${Thread.currentThread().name}")
+    }
+
+}
